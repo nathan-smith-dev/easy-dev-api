1bce899aa0eca139a99292493e8300056c1046f6
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-09-16 23:56:11 -0400

    Boiler plate express app

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const app = require('express')();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const logger = require('./logger');
+
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome home!');
+});
+
+
+const PORT = process.env.PORT || 2121;
+app.listen(PORT, () => logger.info(`Easy Dev API running on port: ${PORT}`));
