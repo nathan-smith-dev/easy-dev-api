29a84063b736c75d2b5d8e7b59d1d38b4ce2f880
Nathan Smith <nis5294@gmail.com>
2018-09-17 20:43:44 -0400

    Added google auth via passport to api

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
+require('./services/passport');
 const app = require('express')();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const log4js = require('log4js');
 const logger = require('./logger');
 
+// logging
 app.use(morgan('tiny'));
+app.use(log4js.connectLogger(log4js.getLogger()));
+
+// middleware
 app.use(bodyParser.json());
+
+// routes
+require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
     res.status(200).send('Home route');
 });
-
 
 const PORT = process.env.PORT || 2121;
 app.listen(PORT, () => logger.info(`Easy Dev API running on port: ${PORT}`));
