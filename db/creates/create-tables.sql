42f12832f3c67e3c647c3a366bbcae2c07a9c310
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-09-20 22:44:12 -0400

    Added SQL to create users table and functions to create and get user

---
+++ db/creates/create-tables.sql
@@ -0,0 +1,9 @@
+﻿-- users 
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+	id VARCHAR(21) UNIQUE NOT NULL, 
+	email VARCHAR(256) NOT NULL, 
+	first_name VARCHAR(35), 
+	last_name VARCHAR(35),
+	last_login TIMESTAMPTZ DEFAULT now()
+);
---
+++ db/creates/functions/create-users-functions.sql
@@ -0,0 +1,29 @@
+﻿DROP FUNCTION IF EXISTS create_user(VARCHAR(21), VARCHAR(256), VARCHAR(35), VARCHAR(35));
+CREATE FUNCTION create_user(id VARCHAR(21), email VARCHAR(256), first_name VARCHAR(35), last_name VARCHAR(35))
+RETURNS users AS 
+$BODY$
+DECLARE usr users;
+BEGIN
+	INSERT INTO users(id, email, first_name, last_name)
+	VALUES(id, email, first_name, last_name)
+	RETURNING * INTO usr;
+
+	RETURN usr;
+END;
+$BODY$
+LANGUAGE PLPGSQL;
+
+DROP FUNCTION IF EXISTS get_user(VARCHAR(21));
+CREATE FUNCTION get_user(usrId VARCHAR(21))
+RETURNS users AS
+$BODY$
+DECLARE usr users;
+BEGIN 
+	SELECT u.id, u.email, u.first_name, u.last_name, u.last_login INTO usr
+	FROM users u
+	WHERE u.id = usrId;
+
+	RETURN usr;
+END; 
+$BODY$
+LANGUAGE PLPGSQL;
