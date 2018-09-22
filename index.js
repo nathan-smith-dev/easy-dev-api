cb8a137a352d97798453202c4e243b3b0ac4d0e7
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-09-21 23:43:49 -0400

    Added serving of static react app on server

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('./services/passport');
 require('./services/db').connect();
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const log4js = require('log4js');
+const path = require('path');
 const logger = require('./logger');
+
+const app = express();
 
 // logging
 app.use(morgan('tiny'));
@@ -32,6 +35,11 @@ require('./routes/authRoutes')(app);
 app.get('/', (req, res) => {
     res.status(200).send('Home route');
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 2121;
 app.listen(PORT, () => logger.info(`Easy Dev API running on port: ${PORT}`));
