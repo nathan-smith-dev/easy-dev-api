16452a15343da92fd4912d02f03ba8d1b17f9e7a
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-09-22 23:10:49 -0400

    Added temporart login logout links to Home page

--- client/src/routes/Home/Home.js
+++ client/src/routes/Home/Home.js
@@ -4,6 +4,12 @@ const home = (props) => {
     return (
         <div>
             <h3>Home Page</h3>
+            <div>
+                <a href="/auth/google">Login</a>
+            </div>
+            <div>
+                <a href="/api/logout">Logout</a>
+            </div>
         </div>
     );
 }
